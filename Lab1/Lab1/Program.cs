d98a1c6f4dc31f638dc11a002ad371b209036fa2
szanko02 <stas.060232@gmail.com>
2021-10-01 00:31:42 +0300

    Create Program.cs

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class MyClass
+{
+    int a = 4;
+    int b = 2;
+    public int A1
+    {
+        get { return a / b; }
+    }
+    public int B1
+    {
+        get { return a; }
+        set { a = value; }
+    }
+    public int C1
+    {
+        get { return b; }
+        set { b = value; }
+    }
+}
+public class Program
+{
+    private static void Main()
+    {
+        MyClass MyObj = new MyClass();
+        Console.WriteLine(MyObj.B1);
+    }
+}
