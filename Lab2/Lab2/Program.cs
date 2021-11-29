0b3187b339b9c2c3b83af1a139c60f0e7f26829e
szanko02 <stas.060232@gmail.com>
2021-11-29 15:13:00 +0300

    Lab2
    2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+public class MyClass
+{
+    protected float a;
+    protected float b;
+
+    public MyClass()
+    {
+        a = 4.11F;
+        b = 21;
+    }
+    public float A1
+    {
+        get { return a / b; }
+    }
+    public float getSetB1
+    {
+        get { return a; }
+        set { a = value; }
+    }
+    public float getSetC1
+    {
+        get { return b; }
+        set { b = value; }
+    }
+}
+
+public class MyClass2: MyClass
+{
+    private const bool f = true;
+    protected int d;
+
+    public MyClass2()
+    {
+        d = 20;
+    }
+    public float getSetC2
+    {
+        get 
+        {
+            if (f) 
+            {
+                return b += d;
+            }
+        }
+    }
+
+}
+public class Program
+{
+    private static void Main()
+    {
+        MyClass MyObj = new MyClass();
+        MyClass2 MyObj2 = new MyClass2();
+        Console.WriteLine(MyObj2.getSetC2);
+        Console.WriteLine(MyObj.A1);
+    }
+}
+
